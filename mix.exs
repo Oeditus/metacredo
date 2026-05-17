1bf40453e1a74f0ab1de1fb578b7645636cff037
Aleksei Matiushkin <am@ambment.cat>
2026-05-17 08:29:19 +0200

    :paperclip: version bump

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule MetaCredo.MixProject do
   use Mix.Project
 
   @app :metacredo
-  @version "0.3.2"
+  @version "0.3.3"
   @source_url "https://github.com/Oeditus/metacredo"
   @homepage_url "https://oeditus.com"
 
