c51b5c763752ab22fbc4ba7ebfba3766a71d1690
Aleksei Matiushkin <am@ambment.cat>
2026-07-26 08:35:40 +0200

    :paperclip: handle unicode in latin1

--- test/metacredo/source_file_test.exs
+++ test/metacredo/source_file_test.exs
@@ -15,7 +15,8 @@ defmodule MetaCredo.SourceFileTest do
 
     test "handles Non-UTF-8 (Latin-1) encoded source code without crashing" do
       # <<241>> is byte 'ñ' in Latin-1 / ISO-8859-1 encoding
-      non_utf8_source = "# Comentario con " <> <<241>> <> "\ndefmodule Latin1Test do\n  def test, do: :ok\nend"
+      non_utf8_source =
+        "# Comentario con " <> <<241>> <> "\ndefmodule Latin1Test do\n  def test, do: :ok\nend"
 
       # Converts Latin-1 ñ (241) to UTF-8 ñ and parses successfully without UnicodeConversionError
       assert {:ok, %SourceFile{} = sf} = SourceFile.parse(non_utf8_source, "latin1.ex", :elixir)
@@ -39,7 +40,10 @@ defmodule MetaCredo.SourceFileTest do
   describe "Sources.find/1" do
     test "safely parses files with non-UTF-8 encoding without crashing task stream" do
       tmp_dir = System.tmp_dir!()
-      file_path = Path.join(tmp_dir, "non_utf8_test_file_#{System.unique_integer([:positive])}.ex")
+
+      file_path =
+        Path.join(tmp_dir, "non_utf8_test_file_#{System.unique_integer([:positive])}.ex")
+
       # Write raw Latin-1 bytes to file
       File.write!(file_path, "# " <> <<241>> <> "\ndefmodule NonUtf8Test do\nend")
 
